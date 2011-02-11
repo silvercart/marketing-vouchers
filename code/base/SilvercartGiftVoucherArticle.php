35be09c37b43a63588a5978ce26103b261fe24bf
Sascha Koehler <skoehler@pixeltricks.de>
2011-02-11 15:18:53 +0100

    Added blueprint voucher type for gift vouchers. Implemented automatic creation of gift vouchers from articles.

--- code/base/SilvercartGiftVoucherArticle.php
+++ code/base/SilvercartGiftVoucherArticle.php
@@ -3,6 +3,8 @@
  * Represents an extended article that generates a
  * SilvercartAbsoluteRebateGiftVoucher object on conversion from shoppingcart
  * to order.
+ * The data for the voucher originates from the related
+ * SilvercartAbsoluteRebateGiftVoucherBlueprint object.
  *
  * @package SilvercartVouchers
  * @author Sascha Koehler <skoehler@pixeltricks.de>
@@ -57,4 +59,103 @@ class SilvercartGiftVoucherArticle extends Article {
         'Title'                                                 => 'Artikel',
         'SilvercartAbsoluteRebateGiftVoucherBlueprint.value'    => 'Gutscheinwert'
     );
+
+    /**
+     * Adjust backend fields.
+     *
+     * @param array $params Additional parameters
+     *
+     * @return FieldSet
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @copyright 2011 pixeltricks GmbH
+     * @since 11.02.2011
+     */
+    public function getCMSFields($params = null) {
+        $fields = parent::getCMSFields($params);
+
+        $fields->removeFieldFromTab('Root', 'shoppingCartPositions');
+        $fields->removeFieldFromTab('Root', 'ArticleAbdaDb');
+        $fields->removeByName('ArticleAbdaLaieninfo');
+        $fields->removeByName('manufacturer');
+        $fields->removeByName('EANCode');
+        $fields->removeByName('ArticleNumberManufacturer');
+        $fields->removeByName('UVP');
+        $fields->removeByName('PurchasePrice');
+        $fields->removeByName('SilvercartAbsoluteRebateGiftVoucherBlueprint');
+
+        $blueprintVouchers = DataObject::get(
+            'SilvercartAbsoluteRebateGiftVoucherBlueprint'
+        );
+        
+        $fields->addFieldToTab(
+            'Root.Main',
+            new DropdownField(
+                'SilvercartAbsoluteRebateGiftVoucherBlueprint',
+                'Gutschein Vorlage',
+                $blueprintVouchers->map('ID', 'value')
+            )
+        );
+
+        return $fields;
+    }
+
+    /**
+     * Shoppingcart hook: gets called when the shopping cart positions get
+     * converted to order positions.
+     *
+     * Here we generate a SilvercartAbsoluteRebateGiftVoucher object and
+     * relate it to our SilvercartAbsoluteRebateGiftVoucherBlueprint object.
+     *
+     * @param Order         $order         The Order object.
+     * @param OrderPosition $orderPosition The OrderPosition object.
+     *
+     * @return void
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @copyright 2011 pixeltricks GmbH
+     * @since 11.02.2011
+     */
+    public function ShoppingCartConvert(Order $order, OrderPosition $orderPosition) {
+        $blueprint = $this->SilvercartAbsoluteRebateGiftVoucherBlueprint();
+
+        if ($blueprint) {
+            // Create gift voucher
+            $giftVoucher = new SilvercartAbsoluteRebateGiftVoucher();
+            //$giftVoucher->generateCode();
+            $giftVoucher->setField('code', 'testtest');
+            $giftVoucher->setField('valueAmount',                                       $blueprint->value->getAmount());
+            $giftVoucher->setField('valueCurrency',                                     $blueprint->value->getCurrency());
+            $giftVoucher->setField('SilvercartAbsoluteRebateGiftVoucherBlueprintID',    $blueprint->ID);
+            $giftVoucher->setField('minimumShoppingCartValueAmount',                    $blueprint->minimumShoppingCartValue->getAmount());
+            $giftVoucher->setField('minimumShoppingCartValueCurrency',                  $blueprint->minimumShoppingCartValue->getCurrency());
+            $giftVoucher->setField('maximumShoppingCartValueAmount',                    $blueprint->maximumShoppingCartValue->getAmount());
+            $giftVoucher->setField('maximumShoppingCartValueCurrency',                  $blueprint->maximumShoppingCartValue->getCurrency());
+            $giftVoucher->setField('TaxID',                                             $blueprint->TaxID);
+            $giftVoucher->write();
+
+            // adjust restrictions
+            foreach ($blueprint->RestrictToMember() as $member) {
+                $giftVoucher->RestrictToMember()->push($member);
+            }
+            foreach ($blueprint->RestrictToGroup() as $group) {
+                $giftVoucher->RestrictToGroup()->push($group);
+            }
+            foreach ($blueprint->RestrictToArticleGroupPage() as $articleGroupPage) {
+                $giftVoucher->RestrictToArticleGroupPage()->push($articleGroupPage);
+            }
+            foreach ($blueprint->RestrictToArticle() as $article) {
+                $giftVoucher->RestrictToArticle()->push($article);
+            }
+            $giftVoucher->write();
+        }
+
+        // Adjust OrderPosition, so that the code gets saved in the order.
+        if (empty($orderPosition->ArticleDescription)) {
+            $description = '';
+        } else {
+            $description = $orderPosition->ArticleDescription."\n\n"."Der Gutschein-Code lautet: ".$giftVoucher->code;
+        }
+        $orderPosition->setField('ArticleDescription', $description);
+    }
 }
---
+++ code/voucher_types/SilvercartAbsoluteRebateGiftVoucherBlueprint.php
@@ -0,0 +1,218 @@
+<?php
+/**
+ * Extends the voucher class for absolute rebates, i.e. 50,00 Eur.
+ *
+ * @package SilvercartVouchers
+ * @author Sascha Koehler <skoehler@pixeltricks.de>
+ * @copyright 2011 pixeltricks GmbH
+ * @since 20.01.2011
+ * @license none
+ */
+class SilvercartAbsoluteRebateGiftVoucherBlueprint extends SilvercartVoucher {
+
+    /**
+     * Singular name
+     *
+     * @var string
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 24.01.2011
+     */
+    public static $singular_name = 'Geschenkgutschein Vorlage';
+
+    /**
+     * Plural name
+     *
+     * @var string
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 24.01.2011
+     */
+    public static $plural_name   = 'Geschenkgutschein Vorlagen';
+
+    /**
+     * Attributes.
+     *
+     * @var array
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 20.01.2011
+     */
+    public static $db = array(
+        'value' => 'Money'
+    );
+
+    /**
+     * Summary fields for the model admin table.
+     *
+     * @var array
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 21.01.2011
+     */
+    public static $summary_fields = array(
+        'quantity'
+    );
+
+    /**
+     * Summary field labels for the model admin.
+     *
+     * @var array
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 21.01.2011
+     */
+    public static $field_labels = array(
+        'quantity'      => 'Anzahl'
+    );
+
+    /**
+     * Has one relationships.
+     *
+     * @var array
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 10.02.2011
+     */
+    public static $has_one = array(
+        'SilvercartGiftVoucherArticle'      => 'SilvercartGiftVoucherArticle',
+        'Customer'                          => 'Member'
+    );
+
+    /**
+     * Has many relationships.
+     *
+     * @var array
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 10.02.2011
+     */
+    public static $has_many = array(
+        'SilvercartAbsoluteRebateGiftVoucher' => 'SilvercartAbsoluteRebateGiftVoucher'
+    );
+
+    // ------------------------------------------------------------------------
+    // Methods
+    // ------------------------------------------------------------------------
+
+    /**
+     * Returns a dataobjectset for the display of the voucher positions in the
+     * shoppingcart.
+     *
+     * @param ShoppingCart $shoppingCart The shoppingcart object
+     * @param Bool         $taxable      Indicates if taxable or nontaxable entries should be returned
+     *
+     * @return DataObjectSet
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @copyright 2011 pixeltricks GmbH
+     * @since 20.01.2011
+     */
+    public function getShoppingCartPositions(ShoppingCart $shoppingCart, $taxable = true) {
+        $controller             = Controller::curr();
+        $removeCartFormRendered = '';
+        $positions              = new DataObjectSet();
+        $tax                    = $this->Tax();
+
+        if ( (!$taxable && !$tax) ||
+             (!$taxable && $tax->Rate == 0) ||
+             ($taxable && $tax && $tax->Rate > 0) ) {
+
+            $removeCartForm = $controller->getRegisteredCustomHtmlForm('SilvercartVoucherRemoveFromCartForm'.$this->ID);
+
+            if ($removeCartForm) {
+                $removeCartForm->setFormFieldValue('VoucherID', $this->ID);
+                $removeCartFormRendered = Controller::curr()->InsertCustomHtmlForm('SilvercartVoucherRemoveFromCartForm'.$this->ID);
+            }
+
+            $positions->push(
+                new DataObject(
+                    array(
+                        'ID'                    => $this->ID,
+                        'Name'                  => self::$singular_name.' (Code: '.$this->code.')',
+                        'ShortDescription'      => $this->code,
+                        'LongDescription'       => $this->code,
+                        'Currency'              => $this->value->getCurrency(),
+                        'Price'                 => $this->value->getAmount() * -1,
+                        'PriceFormatted'        => '-'.$this->value->Nice(),
+                        'PriceTotal'            => $this->value->getAmount() * -1,
+                        'PriceTotalFormatted'   => '-'.$this->value->Nice(),
+                        'Quantity'              => '1',
+                        'removeFromCartForm'    => $removeCartFormRendered,
+                        'TaxRate'               => $this->Tax()->Rate,
+                        'TaxAmount'             => $this->value->getAmount() - ($this->value->getAmount() / (100 + $this->Tax()->Rate) * 100),
+                        'Tax'                   => $this->Tax()
+                    )
+                )
+            );
+        }
+
+        return $positions;
+    }
+
+    /**
+     * Returns the amount to consider in the shopping cart total calculation.
+     *
+     * @return Money
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @copyright 2011 pixeltricks GmbH
+     * @since 24.01.2011
+     */
+    public function getShoppingCartTotal() {
+        $amount = new Money();
+        $member = Member::currentUser();
+
+        $silvercartVoucherShoppingCartPosition = SilvercartVoucherShoppingCartPosition::get($member->shoppingCart()->ID, $this->ID);
+
+        if ($silvercartVoucherShoppingCartPosition &&
+            $silvercartVoucherShoppingCartPosition->implicatePosition) {
+
+            $amount->setAmount($this->value->getAmount() * -1);
+            $amount->setCurrency($this->value->getCurrency());
+        } else {
+            $amount->setAmount(0);
+            $amount->setCurrency($this->value->getCurrency());
+        }
+
+        return $amount;
+    }
+
+    /**
+     * Redefine input fields for the backend.
+     *
+     * @param array params Additional parameters
+     *
+     * @return FieldSet
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @copyright 2011 pixeltricks GmbH
+     * @since 21.01.2011
+     */
+    public function getCMSFields($params = null) {
+        $fields = parent::getCMSFields($params);
+
+        $fields->removeByName('quantityRedeemed');
+        $quantityRedeemedField = new LiteralField('quantityRedeemed', '<br />Eingel&ouml;ste Gutscheine: '.($this->quantityRedeemed ? $this->quantityRedeemed : '0'));
+
+        $fields->addFieldToTab('Root.Main', $quantityRedeemedField);
+
+        // Remove Article Tab and replace with DOM
+        $fields->removeFieldFromTab('Root', 'Articles');
+
+        $articleTable = new HasOneComplexTableField(
+            $this,
+            'Articles',
+            'SilvercartGiftVoucherArticle',
+            Article::$summary_fields,
+            'getCMSFields_forPopup',
+            '',
+            'Article.ID DESC',
+            ''
+        );
+
+        $fields->addFieldToTab('Root.Articles', $articleTable);
+
+        return $fields;
+    }
+}
