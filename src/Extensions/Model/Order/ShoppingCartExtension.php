a7e79bdbf74e220d337afb4a098b2277cf14fbad
Sebastian Diel <sdiel@pixeltricks.de>
2021-04-20 10:51:05 +0200

    MINOR: CMS fields and i18n update.

--- src/Extensions/Model/Order/ShoppingCartExtension.php
+++ src/Extensions/Model/Order/ShoppingCartExtension.php
@@ -3,6 +3,11 @@
 namespace SilverCart\Voucher\Extensions\Model\Order;
 
 use SilverCart\Voucher\Model\ShoppingCartPosition as VoucherShoppingCartPosition;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
 use SilverStripe\ORM\DataExtension;
 
 /**
@@ -27,4 +32,36 @@ class ShoppingCartExtension extends DataExtension
     private static $has_many = [
         'VoucherPositions' => VoucherShoppingCartPosition::class . '.ShoppingCart',
     ];
+    
+    /**
+     * Updates the field labels.
+     * 
+     * @param array &$labels Labels to update
+     * 
+     * @return void
+     */
+    public function updateFieldLabels(&$labels) : void
+    {
+        $labels['VoucherPositions'] = _t(VoucherShoppingCartPosition::class . '.PLURALNAME', 'Voucher Positions');
+    }
+    
+    /**
+     * Updates the CMS fields.
+     * 
+     * @param FieldList $fields Fields
+     * 
+     * @return void
+     */
+    public function updateCMSFields(FieldList $fields) : void
+    {
+        $positionsField = $fields->dataFieldByName('VoucherPositions');
+        if ($positionsField instanceof GridField) {
+            $positionFieldConfig = $positionsField->getConfig();
+            $positionFieldConfig->removeComponentsByType(GridFieldAddExistingAutocompleter::class);
+            $positionFieldConfig->removeComponentsByType(GridFieldAddNewButton::class);
+            $deleteAction        = $positionFieldConfig->getComponentByType(GridFieldDeleteAction::class);
+            /* @var $deleteAction GridFieldDeleteAction */
+            $deleteAction->setRemoveRelation(false);
+        }
+    }
 }
